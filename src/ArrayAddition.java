8b3085ff2284a973c1bae8364ba67b8587673fc0
Hariprashanth75 <56840028+Hariprashanth75@users.noreply.github.com>
2023-05-25 19:56:52 -0300

    First day challenge

---
+++ src/ArrayAddition.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+public class ArrayAddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int arr1[] = {2,4,6,8,10};
+			int arr2[] = {1,3,5,7,9};
+			int arr3[] = new int[arr1.length];
+			
+			for(int i=0;i<arr1.length;i++)
+			{
+			    arr3[i]=arr1[i]+arr2[i];
+			    System.out.println(arr3[i]);
+			}
+
+}
+}
+
---
+++ src/ArrayAdditionInput.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+public class ArrayAdditionInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		System.out.println("Enter the size of the array");
+		n= sc.nextInt();
+		int arr1[] = new int[n];
+		int arr2[] = new int[n];
+		int arr3[] = new int[n];
+		
+		System.out.println("Enter the 1st array elements");
+		for(int i=0;i<arr1.length;i++)
+		{
+			arr1[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the 2nd array elements");
+		for(int j=0;j<arr1.length;j++)
+		{
+			arr2[j] = sc.nextInt();
+		}
+		
+		System.out.println("Sum of two arrays");
+		for(int k=0; k<arr1.length;k++)
+		{
+			arr3[k]=arr1[k]+arr2[k];
+			System.out.println(arr3[k]);
+		}
+
+	}
+
+}
