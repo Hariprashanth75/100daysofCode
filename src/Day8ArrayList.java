23ac9cdb45146a4cfb9de5df6df51d242a6971e2
Hariprashanth75 <56840028+Hariprashanth75@users.noreply.github.com>
2023-06-02 11:06:18 -0300

    Day 8 challenge

---
+++ src/Day8ArrayList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Day8ArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> autos= new ArrayList<String>();
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array size");
+		n = sc.nextInt();
+		System.out.println("Enter the array elements");
+		for(int i=0;i<=n;i++)
+		{
+			String element;
+			element = sc.nextLine();
+			autos.add(element);		
+		}
+		for(int j=0;j<autos.size();j++)
+		{
+		System.out.println(autos.get(j));
+		}
+		sc.close();
+
+	}
+
+}
