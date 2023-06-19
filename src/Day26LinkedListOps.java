853cc8740c300de6937c62f245f52e91d764689b
Hariprashanth75 <56840028+Hariprashanth75@users.noreply.github.com>
2023-06-19 19:33:07 -0300

    Day 26 challenge

---
+++ src/Day26LinkedListOps.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Day26LinkedListOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of cars you want to add");
+		int n = sc.nextInt();
+		LinkedList<String> cars = new LinkedList<String>();
+		
+		System.out.println("Now enter the car names");
+		for(int i=0;i<=n;i++)
+		{
+			String carName = sc.nextLine();
+			cars.add(carName);
+		}
+		System.out.println("The elements of the linked list"+cars);
+		System.out.println(cars.get(1));
+
+	
+		
+
+	}
+
+}
